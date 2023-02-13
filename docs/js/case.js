4f43f6f7d5e1c68e2b6ff904427294f2cff53934
Cy4 <veschi@gmail.com>
2023-02-13 21:29:01 +0000

    Fix case sensitivity

---
+++ docs/js/case.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    currentURL = window.location.href;
+    lowerCaseURL = currentURL.toLowerCase();
+    if (currentURL != lowerCaseURL) {
+        location.replace(lowerCaseURL);
+    }
+};
